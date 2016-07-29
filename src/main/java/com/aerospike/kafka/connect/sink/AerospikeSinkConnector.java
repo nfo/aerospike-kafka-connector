c8448f8d224735af71e1888125a730612a559fa9
Jan Hecking <jhecking@netgaroo.com>
2016-07-29 15:16:09 +0800

    change src dir; set connector version to project.version

--- src/main/java/com/aerospike/kafka/connect/sink/AerospikeSinkConnector.java
+++ src/main/java/com/aerospike/kafka/connect/sink/AerospikeSinkConnector.java
@@ -7,12 +7,13 @@ import java.util.Map;
 
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigException;
-import org.apache.kafka.common.utils.AppInfoParser;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.aerospike.kafka.connect.Version;
 
 public class AerospikeSinkConnector extends SinkConnector {
 
@@ -27,7 +28,7 @@ public class AerospikeSinkConnector extends SinkConnector {
 
 	@Override
 	public void start(Map<String, String> props) {
-		log.trace("Starting {} connector with config: {}", this.getClass().getName(), props);
+		log.info("Starting {} connector", this.getClass().getName());
 		try {
 			configProperties = props;
 			new AerospikeSinkConnectorConfig(props);
@@ -39,7 +40,7 @@ public class AerospikeSinkConnector extends SinkConnector {
 	@Override
 	public void stop() {
 		// Nothing to do since AerospikeSinkConnector has no background monitoring
-		log.trace("Stopping {} connector", this.getClass().getName());
+		log.info("Stopping {} connector", this.getClass().getName());
 	}
 
 	@Override
@@ -60,7 +61,6 @@ public class AerospikeSinkConnector extends SinkConnector {
 
 	@Override
 	public String version() {
-		return AppInfoParser.getVersion();
+		return Version.getVersion();
 	}
-
 }

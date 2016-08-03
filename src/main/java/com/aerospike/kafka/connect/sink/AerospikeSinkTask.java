4dac40bebb44600a4e5bf4a607d865c57dba86c6
Jan Hecking <jhecking@netgaroo.com>
2016-08-04 00:51:42 +0800

    use async client to write records

--- src/main/java/com/aerospike/kafka/connect/sink/AerospikeSinkTask.java
+++ src/main/java/com/aerospike/kafka/connect/sink/AerospikeSinkTask.java
@@ -21,19 +21,14 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.AerospikeException;
 import com.aerospike.client.Bin;
 import com.aerospike.client.Key;
-import com.aerospike.client.policy.ClientPolicy;
-import com.aerospike.client.policy.RecordExistsAction;
-import com.aerospike.client.policy.WritePolicy;
 import com.aerospike.kafka.connect.mapper.KeyAndBins;
 import com.aerospike.kafka.connect.mapper.MappingError;
 import com.aerospike.kafka.connect.mapper.RecordMapper;
@@ -43,8 +38,7 @@ public class AerospikeSinkTask extends SinkTask {
 	private static final Logger log = LoggerFactory.getLogger(AerospikeSinkTask.class);
 	
 	private RecordMapper mapper;
-	private AerospikeClient client;
-	private WritePolicy writePolicy;
+	private AsyncWriter writer;
 
 	public AerospikeSinkTask() {
 	}
@@ -55,8 +49,7 @@ public class AerospikeSinkTask extends SinkTask {
 
 	@Override
 	public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {
-		// TODO Auto-generated method stub
-
+		writer.flush();
 	}
 
 	@Override
@@ -67,7 +60,7 @@ public class AerospikeSinkTask extends SinkTask {
 				Key key = keyAndBins.getKey();
 				Bin[] bins = keyAndBins.getBins();
 				log.trace("Writing record for key {}: {}", key, bins);
-				client.put(writePolicy, key, bins);
+				writer.write(keyAndBins);
 			} catch (MappingError e) {
 				log.error("Error converting record", e);
 			} catch (AerospikeException e) {
@@ -80,16 +73,7 @@ public class AerospikeSinkTask extends SinkTask {
 	public void start(Map<String, String> props) {
 		log.trace("Starting {} task with config: {}", this.getClass().getName(), props);
 		ConnectorConfig config = new ConnectorConfig(props);
-		try {
-			String hostname = config.getHostname();
-			int port = config.getPort();
-			ClientPolicy policy = new ClientPolicy();
-			client = new AerospikeClient(policy, hostname, port);
-		} catch (AerospikeException e) {
-			throw new ConnectException("Could not connect to Aerospike cluster", e);
-		}
-
-		writePolicy = createWritePolicy(config);
+		writer = new AsyncWriter(config);
 		mapper = config.getRecordMapper();
 	}
 
@@ -97,15 +81,4 @@ public class AerospikeSinkTask extends SinkTask {
 	public void stop() {
 		log.trace("Stopping {} task", this.getClass().getName());
 	}
-
-	private WritePolicy createWritePolicy(ConnectorConfig config) {
-		WritePolicy policy = new WritePolicy();
-		RecordExistsAction action = config.getPolicyRecordExistsAction();
-		if (action != null) {
-			policy.recordExistsAction = action;
-		}
-		log.trace("Write Policy: recordExistsAction={}", policy.recordExistsAction);
-		return policy;
-	}
-
 }
---
+++ src/main/java/com/aerospike/kafka/connect/sink/AsyncWriter.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2016 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements WHICH ARE COMPATIBLE WITH THE APACHE LICENSE, VERSION 2.0.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.aerospike.kafka.connect.sink;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.kafka.connect.errors.ConnectException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.async.AsyncClient;
+import com.aerospike.client.listener.WriteListener;
+import com.aerospike.client.policy.RecordExistsAction;
+import com.aerospike.client.policy.WritePolicy;
+import com.aerospike.kafka.connect.mapper.KeyAndBins;
+
+public class AsyncWriter implements WriteListener {
+
+	// TODO: make limit & sleep interval configurable
+	private static final int MAX_ASYNC_TRANSACTIONS = 300;
+	private static final long SLEEP_INTERVAL = 10;
+
+	private static final Logger log = LoggerFactory.getLogger(AsyncWriter.class);
+
+	private AsyncClient client;
+	private WritePolicy writePolicy;
+	private AtomicCounter inFlight = new AtomicCounter(MAX_ASYNC_TRANSACTIONS, SLEEP_INTERVAL);
+
+	public AsyncWriter(ConnectorConfig config) {
+		try {
+			String hostname = config.getHostname();
+			int port = config.getPort();
+			client = new AsyncClient(hostname, port);
+		} catch (AerospikeException e) {
+			throw new ConnectException("Error connecting to Aerospike cluster", e);
+		}
+		writePolicy = createWritePolicy(config);
+	}
+
+	public void write(KeyAndBins keyAndBins) {
+		Key key = keyAndBins.getKey();
+		Bin bins[] = keyAndBins.getBins();
+		inFlight.incr();
+		client.put(writePolicy, this, key, bins);
+	}
+
+	public void flush() {
+		inFlight.waitUntilZero();
+	}
+
+	@Override
+	public void onFailure(AerospikeException e) {
+		log.error("Error writing record", e);
+		inFlight.decr();
+	}
+
+	@Override
+	public void onSuccess(Key key) {
+		log.trace("Successfully put key {}", key);
+		inFlight.decr();
+	}
+
+	private WritePolicy createWritePolicy(ConnectorConfig config) {
+		WritePolicy policy = new WritePolicy();
+		RecordExistsAction action = config.getPolicyRecordExistsAction();
+		if (action != null) {
+			policy.recordExistsAction = action;
+		}
+		log.trace("Write Policy: recordExistsAction={}", policy.recordExistsAction);
+		return policy;
+	}
+	
+	class AtomicCounter {
+		private final int limit;
+		private final long sleepMs;
+		private AtomicInteger counter;
+		
+		public AtomicCounter(int limit, long sleepMs) {
+			this.limit = limit;
+			this.sleepMs = sleepMs;
+			counter = new AtomicInteger(0);
+		}
+		
+		public void incr() {
+			try {
+				int count;
+				while ((count = counter.get()) >= limit) {
+					log.trace("Waiting " + sleepMs + "ms for counter to fall below " + limit + " - current: " + count);
+					Thread.sleep(sleepMs);
+				}
+				counter.incrementAndGet();
+			} catch (InterruptedException e) {
+				throw new ConnectException(e);
+			}
+		}
+		
+		public void decr() {
+			counter.decrementAndGet();
+		}
+		
+		public void waitUntilZero() {
+			try {
+				int count;
+				while ((count = counter.get()) > 0) {
+					log.trace("Waiting " + sleepMs + "ms for counter to reach zero - current: " + count);
+					Thread.sleep(sleepMs);
+				}
+			} catch (InterruptedException e) {
+				throw new ConnectException(e);
+			}
+		}
+	}
+}
